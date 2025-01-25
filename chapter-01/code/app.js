c4c4035897277c29680601b548f58904a7a255bb
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2025-01-25 11:22:23 +0530

    feat: add nested element

--- chapter-01/code/app.js
+++ chapter-01/code/app.js
@@ -1,4 +1,15 @@
-const heading = React.createElement('h1', {id:"heading1"}, 'Hello world using React!')
+// const heading = React.createElement('h1', {id:"heading1"}, 'Hello world using React!')
+const parent=React.createElement("div", {id:"parent"}, [
+    React.createElement("div", {id:"child1"}, [
+        React.createElement("h1", {id:"heading1"}, "Heading1"),
+        React.createElement("h2", {id:"heading2"}, "Heading2"),
+    ]),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {id:"heading1"}, "Heading1"),
+        React.createElement("h2", {id:"heading2"}, "Heading2"),
+    ]),
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
+root.render(parent)
